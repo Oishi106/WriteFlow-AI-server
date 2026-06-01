37e3833a5e0e722fb7e5df3e8b6d360e28bb3553
Oishi106l <mahmudaoishi457@gmail.com>
2026-06-01 23:08:30 +0600

    updated

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -9,11 +9,11 @@ export const asyncHandler = (fn: RequestHandler): RequestHandler => {
 export class AppError extends Error {
   public statusCode: number;
   public isOperational: boolean;
-
+                                                     
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
-    this.isOperational = true;
+    this.isOperational = true;                                                
     Error.captureStackTrace(this, this.constructor);
   }
 }
